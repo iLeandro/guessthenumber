ed87a626debed959732c1922306b2878ce69f0dd
Leandro Almeida <32276456+iLeandro@users.noreply.github.com>
2018-01-27 19:06:12 +0000

    Add files via upload

---
+++ GuessNumber/GuessNumber/Home.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace GuessNumber
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.Transparent;
+            this.btnGuess.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuess.FlatAppearance.BorderSize = 0;
+            this.btnGuess.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnGuess.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuess.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.Transparent;
+            this.btnGuess.Location = new System.Drawing.Point(109, 156);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(148, 37);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNumber.Font = new System.Drawing.Font("Proxima Nova Lt", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(155, 113);
+            this.txtNumber.MaxLength = 3;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(55, 26);
+            this.txtNumber.TabIndex = 2;
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(12, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(17, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackColor = System.Drawing.Color.Transparent;
+            this.btnHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnHelp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Location = new System.Drawing.Point(317, 8);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(43, 17);
+            this.btnHelp.TabIndex = 4;
+            this.btnHelp.UseVisualStyleBackColor = false;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GuessNumber.Properties.Resources.Background;
+            this.ClientSize = new System.Drawing.Size(373, 224);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.btnGuess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ GuessNumber/GuessNumber/Home.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace GuessNumber
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            //int randInt = 0;
+
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+
+            if (Regex.IsMatch(txtNumber.Text.Trim(), "^[a-zA-Z]*$"))
+            {
+                MessageBox.Show("IN DEVELOPMENT");
+            }
+
+            else if (Int32.Parse(txtNumber.Text) < 1 || Int32.Parse(txtNumber.Text) > 100)
+            {
+                MessageBox.Show("Your number has to be between 1 and 100!");
+            }
+            else { if (randomNumber.ToString() == txtNumber.Text) {
+                MessageBox.Show("         Yes. You got it!");
+            } else {
+                MessageBox.Show("No dear, it was " + randomNumber);
+            }
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Try to guess the number I'm thinking!\n  Enter a number between 1 and 100\n                    Good Luck!");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //btnClose. Asks if we want to close the app
+            DialogResult drCloseApp = MessageBox.Show("Do you really want to quit??", "ALERT!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (drCloseApp == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Try to guess the number I'm thinking!\n  Enter a number between 1 and 100\n                    Good Luck!\n\n\n Simple App made in C# by Leandro Almeida\n github.com/iLeandro");
+        }
+    }
+}
